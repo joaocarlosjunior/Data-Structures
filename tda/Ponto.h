2b158b227b9d20c13f077aad835a2078c8077a06
João Carlos <83256465+joaocarlosjunior@users.noreply.github.com>
2021-04-27 03:19:02 -0300

    Update Ponto.h

--- tda/Ponto.h
+++ tda/Ponto.h
@@ -3,7 +3,7 @@
 
 typedef struct ponto Ponto;
 
-Ponto* pto_create(float x,float y);//função para manipular o "Ponto"
+Ponto* pto_create(float x,float y);
 
 void pto_get(Ponto* p, float* x, float* y);
 
